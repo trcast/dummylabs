fae794c42185e9315be824b1679fbc089b2e74a0
trcast <59939928+trcast@users.noreply.github.com>
2023-09-26 16:30:39 -0400

    updated showroom style

--- src/components/Showroom.jsx
+++ src/components/Showroom.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Showroom = React.forwardRef((props, ref) => {
   return (
     <div className="showroom-container" ref={ref}>
-      <div></div>
       <iframe
         className="iframe-showroom"
         src="https://vto.webxr.tools/u/mattjantes/dummy-labs/"
